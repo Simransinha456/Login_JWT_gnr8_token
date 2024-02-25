5e1451e324c47d49791d429e4182b383d5708ee2
Simran Sinha <simransinha280@gmail.com>
2024-02-25 15:01:22 +0530

    commit

--- Routes/routes.js
+++ Routes/routes.js
@@ -4,14 +4,14 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 const routes = express.Router();
- 
+
 // User registration
-routes.post('/register', async (req, res) => {
+routes.post('/register', async (req, res) => { 
     try {
         const { username, password } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = new User({ username, password: hashedPassword });
-        await user.save();
+        await user.save(); //database me save kara rhe
         res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -24,15 +24,18 @@ routes.post('/login', async (req, res) => {
         const { username, password } = req.body;
         const user = await User.findOne({ username });
         if (!user) {
-            return res.status(401).json({ error: 'Authentication failed' });
+            return res.status(401).json({ error: 'User does not exists' });
         }
+
         const passwordMatch = await bcrypt.compare(password, user.password);
         if (!passwordMatch) {
-            return res.status(401).json({ error: 'Authentication failed' });
+            return res.status(401).json({ error: '"Password not matching' });
         }
+
         const token = jwt.sign({ userId: user._id }, 'your-secret-key', { //token generate hoga
             expiresIn: '1h',
         });
+
         res.status(200).json({ token });
     } catch (error) {
         res.status(500).json({ error: error.message });
--- index.js
+++ index.js
@@ -2,40 +2,24 @@ import express, { urlencoded } from "express";
 import mongoose from "mongoose";
 import env from "dotenv"
 import routes from "./Routes/routes.js";
-import postRoutes from "./Routes/postRoutes.js";
 
 env.config()
 const app = express();
+app.use(express.json());//middlewares
 
-// Middlewares
-app.use(express.json());
 
 const mongoUrl = process.env.MONGO_URL;
-// const connect = async () => {
-//     try {
-//         await mongoose.connect(mongoUrl);
-//         console.log("Connected to MongoDB.");
-//     } catch (error) {
-//         console.error("Error connecting to MongoDB:", error);
-//     } 
-// };
-// connect(); 
-
-
-//With self calling function-- called immediate invoke function
-(async () => {
+const connect = async () => {
     try {
         await mongoose.connect(mongoUrl);
         console.log("Connected to MongoDB.");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
-    }
-})();
-
+    } 
+};
+connect(); 
 
 app.use("/", routes); 
-app.use("/", postRoutes);
-
 
 app.listen(8080, () => {
     console.log("Server started on port 8080");
